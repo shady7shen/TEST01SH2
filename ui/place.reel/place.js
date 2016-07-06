a8b7d75666344ff57ee7ba700684a306be75ebd6
shady7shen <noreply>
2016-07-06 01:26:57 +0000

    Add component place

---
+++ ui/place.reel/place.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/place.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Place
+ * @extends Component
+ */
+exports.Place = Component.specialize(/** @lends Place# */ {
+    constructor: {
+        value: function Place() {
+            this.super();
+        }
+    }
+});
